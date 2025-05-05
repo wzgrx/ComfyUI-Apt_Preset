22eb5c1173e3b92a588cf7f2669e46962f40c934
cardenluo <133877069+cardenluo@users.noreply.github.com>
2025-05-06 00:58:38 +0800

    Update Ksampler_all.py

--- NodeChx/Ksampler_all.py
+++ NodeChx/Ksampler_all.py
@@ -42,9 +42,8 @@ from PIL import Image, ImageFilter
 from scipy.stats import norm
 from scipy.ndimage import gaussian_filter, grey_dilation, binary_fill_holes, binary_closing
 from typing import Any, Dict, Optional, Tuple, Union, cast
-import seam_carving
 from comfy.samplers import KSAMPLER
-
+from comfy.model_management import cast_to_device
 
 matplotlib.use('Agg')
 
@@ -2304,71 +2303,6 @@ class img_texture_Offset:
         y = round(y * y_percent / 100)
         x = round(x * x_percent / 100)
         return (pixels.roll((y, x), (1, 2)),)
-
-
-class img_Seam_adjust_size:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "pixels": ("IMAGE",),
-                "width": (
-                    "INT",
-                    {"default": 512},
-                ),
-                "height": (
-                    "INT",
-                    {"default": 512},
-                ),
-            },
-            "optional": {
-                "keep_mask": ("MASK",),
-                "drop_mask": ("MASK",),
-            }
-        }
-
-    RETURN_TYPES = ("IMAGE",)
-    FUNCTION = "run"
-    CATEGORY = "Apt_Preset/imgEffect"
-
-    def run(self, pixels, width, height, keep_mask=None, drop_mask=None):
-        results = []
-
-        if keep_mask is not None:
-            while keep_mask.dim() < 4:
-                keep_mask = keep_mask[None]
-            keep_mask = resize(keep_mask, pixels.size()[1:3])
-
-        if drop_mask is not None:
-            while drop_mask.dim() < 4:
-                drop_mask = drop_mask[None]
-            drop_mask = resize(drop_mask, pixels.size()[1:3])
-
-        for i in range(pixels.size()[0]):
-            image = pixels[i]
-            if keep_mask is not None:
-                k_mask = keep_mask[np.clip(i, 0, keep_mask.size()[0] - 1)][0]
-            else:
-                k_mask = None
-            if drop_mask is not None:
-                d_mask = drop_mask[np.clip(i, 0, drop_mask.size()[0] - 1)][0]
-                if d_mask.any(0).all() or d_mask.any(1).all():
-                    print("SeamCarving: Drop mask would delete entire image, ignoring")
-                    d_mask = None
-            else:
-                d_mask = None
-            src = (255 * image.cpu().clamp(min=0, max=1).numpy()).astype(np.uint8)
-            dst = seam_carving.resize(
-                image,
-                size=(width, height),
-                energy_mode="forward",  # choose from {backward, forward}
-                order="width-first",  # choose from {width-first, height-first}
-                keep_mask=k_mask,  # object mask to protect from removal
-                drop_mask=d_mask,
-                step_ratio=0.1
-            )
-            results.append(torch.from_numpy(dst))
-        return (torch.stack(results),)
 
 
 class DynamicTileSplit:
